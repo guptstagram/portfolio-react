253589d1a8545aba67bf99a9164518f646579572
guptstagram <sourabh.gupta.5095@gmail.com>
2020-09-05 23:58:51 +0530

    skills soon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter,Route, Switch} from "react-router-dom";
 import NavComponent from './components/NavComponent/navComponent';
 import HomeComponent from './components/HomeComponent/homeComponent';
+import SkillsComponent from './components/SkillsComponent/skillsComponent';
 
 class App extends React.Component{
   render(){
@@ -10,8 +11,8 @@ class App extends React.Component{
         <NavComponent/>
         <Switch>
           <Route exact path="/" component={HomeComponent}/>
-          {/* <Route exact path="/skills" component={}/>
-          <Route exact path="/experience" component={}/>
+          <Route exact path="/skills" component={SkillsComponent}/>
+          {/* <Route exact path="/experience" component={}/>
           <Route exact path="/connect" component={}/>
           <Route exact path="/about" component={}/> */}
         </Switch>
---
+++ src/components/SkillsComponent/skillsComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import "./skillsComponent.css"
+
+export class SkillsComponent extends Component {
+    render() {
+        return (
+            <div id="wrapper">
+                <div id="skills-section">
+                    <div class="skills-soon">
+                        <p>Coming Soon...</p>
+                        <p>Don't just wanna put all my skills on skill page but want to use all that to make skills page.</p>
+                        <p>Appreciate your patience.</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SkillsComponent;
