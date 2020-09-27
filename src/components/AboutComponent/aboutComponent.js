1ce16259fe692afadf13867f7bbe21a291071be2
guptstagram <sourabh.gupta.5095@gmail.com>
2020-09-28 01:41:37 +0530

    background image size changes

--- src/components/AboutComponent/aboutComponent.js
+++ src/components/AboutComponent/aboutComponent.js
@@ -12,7 +12,7 @@ export class AboutComponent extends Component {
                         <div id="about-text">
                             <h1>ABOUT ME</h1>
                             <h3>I'm a Programmer in Tata Consultancy Services based in Gandhinagar.</h3>
-                            <p>I have good problem-solving capabilities and expertise in Data Structures and Algorithms.</p>
+                            <p>I have good problem-solving capabilities, expertise in Data Structures and Algorithms and never ending thirst to learn.</p>
                             <p>I love transforming complex problems into simple, beautiful and intuitive code and sometimes designing UI/UX. When I'm not coding, tweeting or pushing pixels, you'll find me writing, making tea, drinking shakes or playing cricket.</p>
                             <div className="resume-button">
                                 <button type="button" onClick={downloadResumeButtonClicked}>Download Resume</button>
--- src/components/ExpComponent/expComponent.js
+++ src/components/ExpComponent/expComponent.js
@@ -14,7 +14,7 @@ export class ExpComponent extends Component {
                 <div id="exp-section">
                     <div className="exp-box">
                         <p>I have <span className="red-text"></span> of experience working as Developer in Tata Consultancy Services on various technologies such as HTML, CSS Flexbox, Bootstrap, JavaScript, Rest APIs, Django, BeautifulSoup.</p>
-                        <p>I have designed and developed various web user interfaces during my tenure in TCS.</p>
+                        <p>I have designed and developed various web apps during my tenure in TCS.</p>
                         <p>I have followed Agile methodologies in my projects and also used tools like Jira, and Git.</p>
                     </div>
                 </div>
