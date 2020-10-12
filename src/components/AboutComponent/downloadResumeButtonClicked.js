813596d49af6a30c4ca305a9c15ab4cdc57b0621
guptstagram <sourabh.gupta.5095@gmail.com>
2020-10-12 21:47:20 +0530

    Update downloadResumeButtonClicked.js

--- src/components/AboutComponent/downloadResumeButtonClicked.js
+++ src/components/AboutComponent/downloadResumeButtonClicked.js
@@ -1,9 +1,6 @@
 // import resume from "../../assets/Resume.pdf";
 
 export const downloadResumeButtonClicked = () => {
-    // window.open(
-    //     {resume}
-    // );
-
+    window.open("https://drive.google.com/file/d/1OlAfrhDX0hF9OJoLkZrOUdEFW9g9KyhJ/view?usp=sharing","_blank");
 };
   
