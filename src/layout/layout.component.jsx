86ab2a731acc04b17e70f8bec65efc34f265e703
SOURABH GUPTA <sourabh.gupta.5095@gmail.com>
2021-07-20 15:17:17 +0530

    particles added

--- src/layout/layout.component.jsx
+++ src/layout/layout.component.jsx
@@ -3,11 +3,20 @@ import "./layout.component.scss";
 
 import { DetailsContext } from "../contexts/myDetailsContext";
 
+import MouseParticles from "react-mouse-particles";
+
 const Layout = ({ children }) => {
   const { nightMode } = React.useContext(DetailsContext);
+
   return (
     <div className={`screen-wrapper ${nightMode ? "night" : "day"}`}>
       <div className={`child-section ${nightMode ? "night" : "day"}`}>
+        <MouseParticles
+          g={10}
+          radius={1}
+          color={["#64ffda", "#ed4457"]}
+          life={1}
+        />
         {children}
       </div>
     </div>
