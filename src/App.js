f7975daa7baa34bb1590cde4c37fc1e351bce383
SOURABH GUPTA <sourabh.gupta.5095@gmail.com>
2021-07-20 16:31:04 +0530

    fonts defer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Nav from './components/nav/nav.component';
+// import Nav from './components/nav/nav.component';
 import Layout from './layout/layout.component';
 import Loader from './components/loader/loader.component';
-
+const Nav = React.lazy(() => import('./components/nav/nav.component'))
 const Home = React.lazy(() => import('./pages/home/home.component'));
 const Expertise = React.lazy(() => import('./pages/expertise/expertise.component'));
 const Experience = React.lazy(() => import('./pages/experience/experience.component'));
@@ -15,18 +15,18 @@ const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Nav />
-        <Switch>
-          <Layout>
-            <React.Suspense fallback={<Loader />}>
+        <Layout>
+          <React.Suspense fallback={<Loader />}>
+            <Nav />
+            <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/expertise" component={Expertise} />
               <Route exact path="/experience" component={Experience} />
               <Route exact path="/connect" component={Connect} />
               <Route exact path="/about" component={About} />
-            </React.Suspense>
-          </Layout>
-        </Switch>
+            </Switch>
+          </React.Suspense>
+        </Layout>
       </BrowserRouter>
     </>
   );
